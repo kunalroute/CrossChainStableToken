38f95c265b9f65782a6852dd54a10191387b036d
kunalroute <kunal@routerprotocol.com>
2022-11-17 22:47:47 +0530

    fix: bridge contracts types

--- middleware/contracts/generic-data-transfer/src/handle_inbound.rs
+++ middleware/contracts/generic-data-transfer/src/handle_inbound.rs
@@ -53,7 +53,6 @@ pub fn handle_in_bound_request(
                 &[
                     ParamType::Uint(256),
                     ParamType::Uint(256),
-                    ParamType::Uint(256),
                     ParamType::Address,
                     ParamType::Address,
                 ],
@@ -67,7 +66,7 @@ pub fn handle_in_bound_request(
                 }
             };
             dest_chain_id = decoded_inner_payload[0].clone().into_uint().unwrap().to_string();
-            dest_contract_address = decoded_inner_payload[4].clone().into_address().unwrap().0;
+            dest_contract_address = decoded_inner_payload[3].clone().into_address().unwrap().0;
         } else if method_type == 1 {
             let decoded_inner_payload = match decode(
                 &[
@@ -124,13 +123,13 @@ pub fn handle_in_bound_request(
         .add_attribute("sender", sender.to_string())
         .add_attribute("src_chain_type", src_chain_type.to_string())
         .add_attribute("src_chain_id", src_chain_id.clone());
-    let sender_contract_address: Vec<u8> = match src_chain_type {
-        0 => {
-            let temp_sender = sender.replace("0x", "");
-            hex::decode(temp_sender).unwrap()
-        }
-        _ => sender.as_bytes().to_vec(),
-    };
+    // let sender_contract_address: Vec<u8> = match src_chain_type {
+    //     0 => {
+    //         let temp_sender = sender.replace("0x", "");
+    //         hex::decode(temp_sender).unwrap()
+    //     }
+    //     _ => sender.as_bytes().to_vec(),
+    // };
 
     // TODO; handle whitelisting
     // if !is_white_listed_contract(
